3c6a188ac21d93a345bf397908fb95ac53d0f227
Laura Rævsbæk Birch <au620747@uni.au.dk>
2020-10-15 10:56:49 +0200

    Layers added

---
+++ Logic_Layer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logic_Layer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
